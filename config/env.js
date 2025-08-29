c7f9c64ac17d9c3bdf5ab7f92fcd1e13246ddca0
1yaz42v <your_email_for_1yaz42v@domain.com>
2025-08-29 19:19:35 +0545

    added arcjet

--- config/env.js
+++ config/env.js
@@ -3,5 +3,5 @@ import { config } from "dotenv";
 // Load environment variables based on NODE_ENV (default = development)
 config({ path: `.env.${process.env.NODE_ENV || "development"}.local` });
 
-export const { PORT, NODE_ENV, DB_URI, JWT_SECRET, JWT_EXPIRES_IN } =
+export const { PORT, NODE_ENV, DB_URI, JWT_SECRET, JWT_EXPIRES_IN, ARCJET_ENV, ARCJET_KEY } =
   process.env;
